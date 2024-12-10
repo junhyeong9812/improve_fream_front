9647b0122c45594f9f84d1bb26710f914de42344
jun <pickjog@gmail.com>
2024-12-10 12:42:46 +0900

    header변경

--- src/components/HeaderFooter/Header.tsx
+++ src/components/HeaderFooter/Header.tsx
@@ -1,43 +1,31 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { FiSearch, FiShoppingBag } from "react-icons/fi";
+import {
+  FiSearch,
+  FiShoppingBag,
+  FiHome,
+  FiBookmark,
+  FiUser,
+} from "react-icons/fi";
 import NotificationModal from "./NotificationModal";
-import SearchModal from "./SearchModal"; // SearchModal 추가
+import SearchModal from "./SearchModal";
 
+// 기본 데스크탑 헤더 컨테이너
 const HeaderContainer = styled.header`
-  display: flex;
-  flex-direction: column;
-  //   background-color: #fff;
-  border-bottom: 1px solid #ddd;
-`;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1280px;
+  min-width: 960px;
 
-const TopNav = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 5px 20px;
-  background-color: #f8f8f8;
-
-  a {
-    text-decoration: none;
-    color: #555;
-    font-size: 12px; /* 글자 크기 절반으로 줄임 */
-    margin-left: 15px;
-    font-weight: 500;
-
-    &:hover {
-      color: #000;
-    }
+  @media (max-width: 960px) {
+    display: none; /* 모바일 환경에서는 숨김 */
   }
 `;
-
-const BottomNav = styled.div`
+// Nav 컴포넌트 스타일 정의
+const Nav = styled.nav`
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  padding: 10px 20px;
-  background: none; /* 배경 제거 */
 
   a {
     text-decoration: none;
@@ -49,19 +37,146 @@ const BottomNav = styled.div`
       color: #000;
     }
   }
-`;
 
-const Logo = styled.div`
-  img {
-    width: 100px;
+  @media (max-width: 960px) {
+    display: none; /* 데스크톱 전용 */
   }
 `;
 
-const Nav = styled.nav`
-  display: flex;
-  align-items: center;
+// 모바일 전용 헤더 컨테이너
+const MobileHeaderContainer = styled.header`
+  display: none;
+
+  @media (max-width: 960px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #fff;
+    border-bottom: 1px solid #ddd;
+    z-index: 1000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    min-width: 320px;
+  }
 `;
 
+const TopNav = styled.div`
+  display: flex;
+  width: 100%;
+  height: 22px;
+  padding: 8px 40px 0;
+  justify-content: flex-end;
+  align-items: center;
+  box-sizing: border-box;
+
+  ul {
+    list-style: none;
+    display: flex;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-left: 15px;
+
+    a {
+      text-decoration: none;
+      color: #555;
+      font-size: 12px;
+      font-weight: 500;
+
+      &:hover {
+        color: #000;
+      }
+    }
+  }
+`;
+
+const BottomNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 64px;
+  justify-content: space-between;
+  min-width: 320px;
+  padding: 20px 40px;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+const MobileNav = styled.nav`
+  display: none;
+
+  @media (max-width: 960px) {
+    display: flex;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: calc(50px + env(safe-area-inset-bottom));
+    background-color: #fff;
+    box-shadow: 0 -0.5px 0 0 rgba(34, 34, 34, 0.1);
+    z-index: 1000;
+    padding-bottom: env(safe-area-inset-bottom);
+    min-width: 320px;
+
+    ul {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      height: 100%;
+    }
+
+    li {
+      flex: 1;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    a {
+      text-decoration: none;
+      color: #222;
+      font-size: 12px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      svg {
+        font-size: 20px;
+        margin-bottom: 4px;
+      }
+
+      &:hover {
+        color: #000;
+      }
+    }
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 100px;
+  background: none;
+  display: block;
+
+  @media (max-width: 960px) {
+    width: 80px; /* 모바일 로고 크기 조정 */
+  }
+`;
+
+const MobileLogoImage = styled.img`
+  width: 80px; /* 모바일 전용 로고 */
+  display: block;
+`;
+
+// 아이콘 버튼 스타일
 const Icon = styled.button`
   background: none;
   border: none;
@@ -72,59 +187,110 @@ const Icon = styled.button`
   color: #222;
   margin-left: 10px;
 `;
-const LogoImage = styled.img`
-  width: 100px;
-  background: none;
-  display: block;
-`;
 
+// 컴포넌트
 const Header: React.FC = () => {
   const [isNotificationModalOpen, setNotificationModalOpen] = useState(false);
-  const [isSearchModalOpen, setSearchModalOpen] = useState(false); // SearchModal 상태 추가
+  const [isSearchModalOpen, setSearchModalOpen] = useState(false);
 
   return (
-    <HeaderContainer>
-      {/* 상단 네비게이션 */}
-      <TopNav>
-        <Link to="/support">고객센터</Link>
-        <Link to="/mypage">마이페이지</Link>
-        <Link to="/favorites">관심</Link>
-        <Link to="#" onClick={() => setNotificationModalOpen(true)}>
-          알림
-        </Link>
-        <Link to="/login">로그인</Link>
-      </TopNav>
+    <>
+      {/* 기본 데스크탑 헤더 */}
+      <HeaderContainer>
+        <TopNav>
+          <ul>
+            <li>
+              <Link to="/support">고객센터</Link>
+            </li>
+            <li>
+              <Link to="/mypage">마이페이지</Link>
+            </li>
+            <li>
+              <Link to="/favorites">관심</Link>
+            </li>
+            <li>
+              <Link to="#" onClick={() => setNotificationModalOpen(true)}>
+                알림
+              </Link>
+            </li>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+          </ul>
+        </TopNav>
 
-      {/* 하단 네비게이션 */}
-      <BottomNav>
-        <Logo>
+        <BottomNav>
           <Link to="/">
-            {/* <img src="/Fream.png" alt="KREAM Logo" /> */}
             <LogoImage src="/Fream.png" alt="KREAM Logo" />
           </Link>
-        </Logo>
-        <Nav>
-          <Link to="/">HOME</Link>
-          <Link to="/style">STYLE</Link>
-          <Link to="/shop">SHOP</Link>
-          <Icon onClick={() => setSearchModalOpen(true)}>
-            {" "}
-            {/* 돋보기 클릭 */}
-            <FiSearch />
-          </Icon>
-          <Icon>
-            <FiShoppingBag />
-          </Icon>
-        </Nav>
-      </BottomNav>
+          <Nav>
+            <Link to="/">HOME</Link>
+            <Link to="/style">STYLE</Link>
+            <Link to="/shop">SHOP</Link>
+            <Icon onClick={() => setSearchModalOpen(true)}>
+              <FiSearch />
+            </Icon>
+            <Icon>
+              <FiShoppingBag />
+            </Icon>
+          </Nav>
+        </BottomNav>
+      </HeaderContainer>
 
+      {/* 모바일 전용 헤더 */}
+      <MobileHeaderContainer>
+        <Link to="/">
+          <MobileLogoImage src="/Fream.png" alt="Mobile Logo" />
+        </Link>
+        <Icon onClick={() => setNotificationModalOpen(true)}>
+          <FiShoppingBag />
+        </Icon>
+      </MobileHeaderContainer>
+
+      {/* 모바일 전용 하단 네비게이션 */}
+      <MobileNav>
+        <ul>
+          <li>
+            <Link to="/">
+              <FiHome />
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/style">
+              <FiBookmark />
+              STYLE
+            </Link>
+          </li>
+          <li>
+            <Link to="/shop">
+              <FiSearch />
+              SHOP
+            </Link>
+          </li>
+          <li>
+            <Link to="/saved">
+              <FiBookmark />
+              SAVED
+            </Link>
+          </li>
+          <li>
+            <Link to="/mypage">
+              <FiUser />
+              MY
+            </Link>
+          </li>
+        </ul>
+      </MobileNav>
+
+      {/* 모달 */}
       {isNotificationModalOpen && (
         <NotificationModal closeModal={() => setNotificationModalOpen(false)} />
       )}
       {isSearchModalOpen && (
         <SearchModal closeModal={() => setSearchModalOpen(false)} />
       )}
-    </HeaderContainer>
+    </>
   );
 };
 
