f0db7f46867ee0f0a2837c5aeff19deab27294e3
jun <pickjog@gmail.com>
2025-01-31 23:14:45 +0900

    build check

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1>Kream사이트</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
