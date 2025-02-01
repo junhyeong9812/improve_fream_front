c62368cf5dcd78f5176d58257d2076a40229accb
jun <pickjog@gmail.com>
2025-02-02 02:24:32 +0900

    weather Error

--- src/global/components/header/services/weatherService.ts
+++ src/global/components/header/services/weatherService.ts
@@ -3,7 +3,7 @@ import { WeatherDataDto } from "src/global/types/weather";
 
 export async function fetchCurrentWeather(): Promise<WeatherDataDto | null> {
   try {
-    const response = await api.get<WeatherDataDto>("/weather/current");
+    const response = await api.get<WeatherDataDto>("/weather/query/current");
     return response.data;
   } catch (err) {
     console.error("Failed to fetch current weather:", err);
